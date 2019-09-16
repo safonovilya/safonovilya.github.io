4c8ecf29d91bf682b950b4c136d8a5fd6322bf5e
Ilya Safonov <safonov.ilya@gmail.com>
2019-09-16 16:54:30 +0300

    clock publish

--- js/clock.js
+++ js/clock.js
@@ -1,81 +1,382 @@
-
-
 const NumbersMap = {
-    1: [
-        {"h": "90",  "m": "180"},
-        {"h": "270", "m": "90"},
-        {"h": "270", "m": "180"},
-        {"h": "225", "m": "225"},
+  0: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
 
-        {"h": "90", "m": "0"},
-        {"h": "180",  "m": "270"},
-        {"h": "180", "m": "0"},
-        {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
 
-        {"h": "225", "m": "225"},
-        {"h": "180", "m": "0"},
-        {"h": "180", "m": "0"},
-        {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
 
-        {"h": "225", "m": "225"},
-        {"h": "180", "m": "0"},
-        {"h": "180", "m": "0"},
-        {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
 
-        {"h": "90",  "m": "180"},
-        {"h": "0", "m": "270"},
-        {"h": "0", "m": "90"},
-        {"h": "180", "m": "270"},
 
-        {"h": "90", "m": "0"},
-        {"h": "90", "m": "270"},
-        {"h": "90", "m": "270"},
-        {"h": "0",  "m": "270"}
-    ],
-    8: [
-        {"h": "90",  "m": "180"},
-        {"h": "90",  "m": "270"},
-        {"h": "90",  "m": "270"},
-        {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "0", "m": "180"},
 
-        {"h": "180", "m": "0"},
-        {"h": "90",  "m": "180"},
-        {"h": "180", "m": "270"},
-        {"h": "180", "m": "0"},
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  1: [
+    {"h": "90", "m": "180"},
+    {"h": "270", "m": "90"},
+    {"h": "270", "m": "180"},
+    {"h": "225", "m": "225"},
 
-        {"h": "0", "m": "135"},
-        {"h": "0", "m": "90"},
-        {"h": "0", "m": "270"},
-        {"h": "0", "m": "225"},
+    {"h": "90", "m": "0"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+    {"h": "225", "m": "225"},
 
-        {"h": "45", "m": "180"},
-        {"h": "90",  "m": "180"},
-        {"h": "180", "m": "270"},
-        {"h": "315", "m": "180"},
+    {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "225", "m": "225"},
 
-        {"h": "180", "m": "0"},
-        {"h": "0", "m": "90"},
-        {"h": "0", "m": "270"},
-        {"h": "0", "m": "180"},
+    {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "225", "m": "225"},
 
-        {"h": "90", "m": "0"},
-        {"h": "90", "m": "270"},
-        {"h": "90", "m": "270"},
-        {"h": "0",  "m": "270"}
-    ]
+    {"h": "90", "m": "180"},
+    {"h": "0", "m": "270"},
+    {"h": "0", "m": "90"},
+    {"h": "180", "m": "270"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  2: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "180"},
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  3: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "0", "m": "180"},
+
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  4: [
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "0", "m": "180"},
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "0", "m": "180"},
+    {"h": "180", "m": "0"},
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "90", "m": "0"},
+    {"h": "0", "m": "270"}
+  ],
+  5: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "0", "m": "180"},
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  6: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "0", "m": "180"},
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  7: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "180", "m": "0"},
+
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "180"},
+
+    {"h": "225", "m": "225"},
+    {"h": "225", "m": "225"},
+    {"h": "90", "m": "0"},
+    {"h": "0", "m": "270"}
+  ],
+  8: [
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "135"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "0", "m": "225"},
+
+    {"h": "45", "m": "180"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "315", "m": "180"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "0", "m": "180"},
+
+    {"h": "90", "m": "0"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ],
+  9: [
+    {"h": "90", "m": "180"},
+    {"h": "270", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+
+    {"h": "180", "m": "0"},
+    {"h": "90", "m": "180"},
+    {"h": "180", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "180", "m": "0"},
+    {"h": "0", "m": "90"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "180", "m": "270"},
+    {"h": "0", "m": "180"},
+
+    {"h": "90", "m": "180"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"},
+    {"h": "180", "m": "0"},
+
+    {"h": "0", "m": "90"},
+    {"h": "90", "m": "270"},
+    {"h": "90", "m": "270"},
+    {"h": "0", "m": "270"}
+  ]
 };
+const CLOCK_ELEMENT_CLASS_NAME = 'clock';
+const CLOCK_CONTAINER_CLASS_NAME = 'clock-container';
+const elements = document.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME);
+const ALL_CLASS_NAMES_ARRAY = [0, 45, 90, 135, 180, 225, 270, 315].map(c => `deg${c}`);
+let number = 0;
 
-const elements = document.getElementsByTagName("article");
-const allClassList = [0,45,90,135,180,225,270,315].map(c=> `deg${c}`);
-const number = 8;
-setTimeout(()=> {
-    for (let i=0; i< elements.length; i++) {
-        const el = elements[i];
-        // debugger;
-        el.getElementsByClassName("hours-container")[0].classList.remove(...allClassList);
-        el.getElementsByClassName("minutes-container")[0].classList.remove(...allClassList);
-        el.getElementsByClassName("hours-container")[0].classList.add(`deg${NumbersMap[number][i].h}`);
-        el.getElementsByClassName("minutes-container")[0].classList.add(`deg${NumbersMap[number][i].m}`);
-    }
+function init(){
+  // set Timeout
+  buildClock();
 
-}, 2000);
+  const [h1, h2, m1, m2, s1, s2] = document.getElementsByClassName(CLOCK_CONTAINER_CLASS_NAME);
+  let [nh1, nh2] = returnTwoDigits(new Date().getHours());
+  let [nm1, nm2] = returnTwoDigits(new Date().getMinutes());
+  let [ns1, ns2] = returnTwoDigits(new Date().getSeconds());
+
+  updateClasses(h1.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), nh1);
+  updateClasses(h2.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), nh2);
+  updateClasses(m1.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), nm1);
+  updateClasses(m2.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), nm2);
+  updateClasses(s1.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), ns1);
+  updateClasses(s2.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), ns2);
+
+  setInterval(() => {
+    [ns1, ns2] = returnTwoDigits(new Date().getSeconds());
+    updateClasses(s1.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), ns1);
+    updateClasses(s2.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME), ns2);
+  }, 1000);
+}
+
+function returnTwoDigits(number) {
+  const second = number % 10;
+  const first = ((number - second) / 10) % 10;
+  return [first, second];
+}
+
+function buildClock() {
+
+  // fill all containers by clocks
+  const containerElement = document.getElementsByClassName(CLOCK_CONTAINER_CLASS_NAME)[0];
+  const itm = document.getElementsByClassName(CLOCK_ELEMENT_CLASS_NAME)[0];
+  fillContainerByClocks(containerElement, itm);
+
+  // make 4 containers
+  containerElement.parentElement.appendChild(containerElement.cloneNode(true));
+  containerElement.parentElement.appendChild(containerElement.cloneNode(true));
+  containerElement.parentElement.appendChild(containerElement.cloneNode(true));
+  containerElement.parentElement.appendChild(containerElement.cloneNode(true));
+  containerElement.parentElement.appendChild(containerElement.cloneNode(true));
+}
+
+function fillContainerByClocks( containerElement, clockElement) {
+  for (let i = 0; i < NumbersMap[0].length - 1; i++) {
+    const cln = clockElement.cloneNode(true);
+    containerElement.appendChild(cln);
+  }
+}
+function updateClasses(elements, number) {
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    // console.log(number, i, NumbersMap[number],NumbersMap[number][i])
+    el.getElementsByClassName("hours-container")[0].classList.remove(...ALL_CLASS_NAMES_ARRAY);
+    el.getElementsByClassName("minutes-container")[0].classList.remove(...ALL_CLASS_NAMES_ARRAY);
+    el.getElementsByClassName("hours-container")[0].classList.add(`deg${NumbersMap[number][i].h}`);
+    el.getElementsByClassName("minutes-container")[0].classList.add(`deg${NumbersMap[number][i].m}`);
+  }
+}
+
+init();
